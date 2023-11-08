4f50bba8a043e142c84133b6cb9417e652168fce
kruminskr <kruminskr@gmail.com>
2023-11-08 20:53:48 +0200

    feat: Add data validation, refactor input handling

--- src/bookInventory.py
+++ src/bookInventory.py
@@ -23,18 +23,20 @@ bookInventory = {
 }
 
 def addBook (ISBN, title, author, price, quantity):
-        bookInventory[ISBN] = {
-            "title": title,
-            "author": author,
-            "ISBN": ISBN,
-            "price": price,
-            "quantity": quantity
-        }
+     bookInventory[ISBN] = {
+        "title": title,
+        "author": author,
+        "ISBN": ISBN,
+        "price": price,
+        "quantity": quantity
+     }
+     return
 
 
 def removeBook (ISBN):
      if ISBN in bookInventory:
         del bookInventory[ISBN]
+     return
 
 def searchBookByISBN (ISBN):
     if ISBN in bookInventory:
@@ -42,13 +44,63 @@ def searchBookByISBN (ISBN):
     return 
 
 def seacrhBookByTitleOrAuthor (query):
-    matching_books = []
+    matchingBooks = []
     for book in bookInventory.values():
         if query.lower() in book["title"].lower() or query.lower() in book["author"].lower():
-            matching_books.append(book)
-    return matching_books
+            matchingBooks.append(book)
+    return matchingBooks
 
-# to-do
-# testing
-# parnest datu validaciju uz atsevisku funkciju
-# print kad tiek searchots
+def displayBook (ISBN):
+    print(f"ISBN: {ISBN['ISBN']}")
+    print(f"Title: {ISBN['title']}")
+    print(f"Author: {ISBN['author']}")
+    print(f"Price: {ISBN['price']}")
+    print(f"Quantity: {ISBN['quantity']}")
+
+def validateISBN (ISBN):
+    while not ISBN.isdigit():
+        print("Invalid ISBN. Please enter a numeric value.")
+        ISBN = input("ISBN: ")
+    return ISBN
+
+def validateTitle (title):
+    while not title:
+        print("Title cannot be empty. Please enter a title.")
+        title = input("Title: ")
+    return title
+
+def validateAuthor (author):
+    while not author:
+        print("Author cannot be empty. Please enter an author name.")
+        author = input("Author: ")
+    return author
+
+def validatePrice (price):
+    while True:
+        try:
+            price = float(input("Price: "))
+            if price < 0:
+                print("Price cannot be negative. Please enter a positive value.")
+            else:
+                break
+        except ValueError:
+            print("Invalid price. Please enter a numeric value.")
+        return price
+
+def validateQuantity (quantity):
+    while True:
+        try:
+            quantity = int(input("Quantity: "))
+            if quantity < 0:
+                print("Quantity cannot be negative. Please enter a positive value.")
+            else:
+                break
+        except ValueError:
+            print("Invalid quantity. Please enter a numeric value.")
+    return quantity
+
+def validateQuery (query):
+    while not query:
+        print("Query cannot be empty. Please enter an author or title.")
+        query = input("query: ")
+    return query
